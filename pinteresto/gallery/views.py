d8374fc1f138436ad19aa27d132fe362f2855eb2
9pedobear <9pedobear@gmail.com>
2022-01-12 20:43:00 +0600

    init/new_branch/devs

--- pinteresto/gallery/views.py
+++ pinteresto/gallery/views.py
@@ -3,3 +3,34 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'gallery/index.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
