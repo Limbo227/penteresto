b2c50f71ea58c3a6d49d3d56a3664dc16539ee72
Bakai <bakai.ergeshev@gmail.com>
2022-01-18 20:29:15 +0600

    vot eto rabotaet

--- pinterest/pinteresto/pinteresto/urls.py
+++ pinterest/pinteresto/pinteresto/urls.py
@@ -19,7 +19,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gallery.urls')),
-    path('user/', include('user.urls')),
+    path('', include('user.urls')),
+    path('', include('gallery.urls')),
 ]
 
 
